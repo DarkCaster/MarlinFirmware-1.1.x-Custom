3925cc782c57494948742adc123ced37d71bfa9c
DarkCaster <dark.caster@outlook.com>
2018-09-30 23:36:25 +0300

    Configuration.h updated, PREHEAT_1_TEMP_HOTEND, PREHEAT_1_TEMP_HOTEND, PREHEAT_2_TEMP_BED updated

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1261,12 +1261,12 @@
 // @section temperature
 
 // Preheat Constants
-#define PREHEAT_1_TEMP_HOTEND 210
+#define PREHEAT_1_TEMP_HOTEND 190
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_TEMP_HOTEND 235
-#define PREHEAT_2_TEMP_BED    110
+#define PREHEAT_2_TEMP_HOTEND 215
+#define PREHEAT_2_TEMP_BED    100
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
