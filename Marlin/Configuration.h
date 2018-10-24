308555513bd58d0ac90dbbd48cf39da65a69ada7
DarkCaster <dark.caster@outlook.com>
2018-10-24 03:17:35 +0300

    Configuration.h updated, minor fixes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1077,7 +1077,7 @@
 
   #define MESH_INSET 0              // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 5      // Don't use more than 15 points per axis, implementation limited.
-  #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
+  #define GRID_MAX_POINTS_Y 5
 
   #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
   #define UBL_SAVE_ACTIVE_ON_M500   // Save the currently active mesh in the current slot on M500
