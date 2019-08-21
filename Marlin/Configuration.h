d4ff137fcd19426508ab769f977231c99cd128a9
DarkCaster <dark.caster@outlook.com>
2019-08-21 23:38:05 +0300

    Configuration_adv.h updated, set X_BED_SIZE to 181mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -893,7 +893,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 180
+#define X_BED_SIZE 181
 #define Y_BED_SIZE 167
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
